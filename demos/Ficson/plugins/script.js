1243427a1d279da73edb7a63c321978df4128a66
hanslindetorp <hans@melos.se>
2021-11-17 01:41:06 +0100

    Ficson update

--- demos/Ficson/plugins/script.js
+++ demos/Ficson/plugins/script.js
@@ -1,22 +1,6 @@
-// const recursiveCall = (obj) => {
-//     return new Promise((resolve) => {
-//         if(obj instanceof Array){
-//             let cnt = obj.length;
-//             obj.forEach(el => {
-//                 recursiveCall(el)
-//                 .then(() => {
-//                     if(!--cnt)return resolve();
-//                 })
-                
-//             });
-//         } else {
-//             setTimeout(() => {
-//                 console.log(obj);
-//                 return resolve();
-//             }, obj);
-            
-//         }
-//     })
-// }
-
-// recursiveCall([100,50,[10,[200, 300],30],1]).then(() => console.log('done'));
+window.addEventListener("load", e => {
+    let video = document.querySelector("video");
+    setInterval(() => {
+        video.dispatchEvent(new CustomEvent("timeupdate"));
+    }, 50)
+});
