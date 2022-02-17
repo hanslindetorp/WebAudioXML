878697c9a47c6ec08c66e433f1f764acb6a35b2d
hanslindetorp <hans@melos.se>
2022-02-17 10:05:58 +0100

    Instructions updated

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ webAudioXML.addEventListener("init", e => {
     webAudioXML.mute();
     
     document.querySelector("#playBtn").addEventListener("click", e => {
-        webAudioXML.unmute();
+        webAudioXML.unmute(0.01);
     });
     document.querySelector("#stopBtn").addEventListener("click", e => {
-        webAudioXML.mute();
+        webAudioXML.mute(0.01);
     });
 });
