dedd39de3f01fbf1c355ea309dc1639336d601b0
hanslindetorp <hans@melos.se>
2023-10-24 17:02:04 -0400

    Removed

--- finderjs/example/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var exampleStatic = require('./example-static');
-var exampleStaticSelectPath = require('./example-static-select-path');
-var exampleAsync = require('./example-async');
-
-exampleStatic(document.getElementById('container1'));
-exampleAsync(document.getElementById('container2'));
-exampleStaticSelectPath(document.getElementById('container3'));
