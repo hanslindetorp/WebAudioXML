a4c1c9dc2f355c9e580aaf0d78ff7f0731cdb457
hanslindetorp <hans@melos.se>
2024-11-22 14:10:52 +0100

    Dispatched events on ended

--- Classes/AudioObject.js
+++ Classes/AudioObject.js
@@ -79,6 +79,10 @@ class AudioObject extends EventTarget{
 
 		  	case "audiobuffersourcenode":
         this._node = new BufferSourceObject(this, params);
+        this._node.addEventListener("ended", e => {
+          this.dispatchEvent(new CustomEvent("ended"));
+        });
+        this.dispatchEvent(new CustomEvent("loadComplete"));
         // this.bufferSource = new BufferSourceObject(this._ctx, params);
         // creates a living connection with the current active buffernode
 		  	// this._node = this.bufferSource._node;
@@ -263,6 +267,14 @@ class AudioObject extends EventTarget{
 
         case "objectbasedaudio":
         this._node = new ObjectBasedAudio(this, this._params, waxml);
+        this._node.addEventListener("ended", e => {
+          this.dispatchEvent(new CustomEvent("ended"));
+        });
+        this._node.addEventListener("loadComplete", e => {
+          this.dispatchEvent(new CustomEvent("loadComplete", {
+            detail: e.detail
+          }));
+        });
         break;
 
         case "ambientaudio":
@@ -538,7 +550,11 @@ class AudioObject extends EventTarget{
    
             let v = this._params[key].valueOf();
             // if(typeof v !== "undefined")this[key] = v;
-            if(typeof v == typeof this[key] || typeof this[key] == "undefined")this[key] = v;
+            if(typeof v != "undefined"){
+              if(typeof v == typeof this[key] || typeof this[key] == "undefined"){
+                this[key] = v;
+              }
+            }
           }
 
   			});
@@ -987,7 +1003,7 @@ class AudioObject extends EventTarget{
         }
 
   	  	if(transitionTime && param.setTargetAtTime){
-  		  	param.setTargetAtTime(value, startTime, transitionTime / 2);
+  		  	param.setTargetAtTime(value, startTime, transitionTime);
           // console.log(`transitionTime: ${transitionTime}`);
         } else if(param.setValueAtTime){
   		  	param.setValueAtTime(value, startTime);
